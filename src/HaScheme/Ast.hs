cc335496e4958d3a73bac74c8970d6acef83bbc5
Paweł Rubin <pawelrubin19@gmail.com>
2020-12-07 22:49:18 +0100

    Simple AST with pretty instance.

---
+++ src/HaScheme/Ast.hs
@@ -0,0 +1,25 @@
+module HaScheme.Ast where
+
+import Data.Text (Text)
+import Data.Text.Prettyprint.Doc
+
+data SchemeVal
+  = Atom Text
+  | List [SchemeVal]
+  | Number Integer
+  | String Text
+  | Bool Bool
+  | Nil
+  deriving (Show, Eq)
+
+--------------------------------------------
+-- Pretty instances
+--------------------------------------------
+instance Pretty SchemeVal where
+  pretty = \case
+    Atom name -> pretty name
+    List values -> lparen <> hsep (map (\val -> pretty val <> "") values) <> rparen
+    Number num -> pretty num
+    String str -> pretty str
+    Bool b -> pretty b
+    Nil -> "()"
